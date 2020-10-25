d7b6c35dc5bd8e58a921b1c33f5ccd4dbec686af
wskrepobackup1 <69301723+wskrepobackup1@users.noreply.github.com>
2020-10-25 17:15:52 +0800

    1.0.5
    by pengzhile upload by me
    https://gitee.com/pengzhile/ide-eval-resetter.git

--- reset_eval/reset_jetbrains_eval_mac_linux.sh
+++ reset_eval/reset_jetbrains_eval_mac_linux.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# reset jetbrains ide evals v1.0.4
+# reset jetbrains ide evals v1.0.5
 
 OS_NAME=$(uname -s)
 JB_PRODUCTS="IntelliJIdea CLion PhpStorm GoLand PyCharm WebStorm Rider DataGrip RubyMine AppCode"
@@ -14,6 +14,7 @@ if [ "$OS_NAME" == "Darwin" ]; then
     sed -i '' '/name="evlsprt.*"/d' ~/Library/Application\ Support/JetBrains/"${PRD}"*/options/other.xml >/dev/null 2>&1
   done
 
+  rm -rf ~/Library/Preferences/jetbrains.*.*.plist
   plutil -remove "/.JetBrains\.UserIdOnMachine" ~/Library/Preferences/com.apple.java.util.prefs.plist >/dev/null
   plutil -remove "/.jetbrains/.user_id_on_machine" ~/Library/Preferences/com.apple.java.util.prefs.plist >/dev/null
   plutil -remove "/.jetbrains/.device_id" ~/Library/Preferences/com.apple.java.util.prefs.plist >/dev/null
@@ -30,6 +31,7 @@ elif [ "$OS_NAME" == "Linux" ]; then
   sed -i '/key="JetBrains\.UserIdOnMachine"/d' ~/.java/.userPrefs/prefs.xml
   sed -i '/key="device_id"/d' ~/.java/.userPrefs/jetbrains/prefs.xml
   sed -i '/key="user_id_on_machine"/d' ~/.java/.userPrefs/jetbrains/prefs.xml
+  find ~/.java/.userPrefs/jetbrains/ -name "evlsprt*" -type d -exec rm -rf {} \; >/dev/null 2>&1
 else
   echo 'unsupport'
   exit
